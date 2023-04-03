a37440a998fc599c98034e58442cb540afe61401
ammwangi <elijah.mwangi@kamsware.com>
2023-04-03 13:39:02 +0300

    validations

--- src/SecondsAndMinutesChallenge.java
+++ src/SecondsAndMinutesChallenge.java
@@ -1,22 +1,35 @@
 public class SecondsAndMinutesChallenge {
 
     public static void main(String[] args) {
-        System.out.println(getDurationString(3945)); //First Case
-        System.out.println(getDurationString(65, 45)); //Second case
+        System.out.println(getDurationString(-3945)); //First Case
+        System.out.println(getDurationString(-65, 45)); //Second case
+        System.out.println(getDurationString(65, 145)); //Second case\
+
+        System.out.println(getDurationString(65, 45));
+        System.out.println(getDurationString(3945));
+
     }
 
         public static String getDurationString(int seconds){
        //return  'XXh YYm ZZs' where xx is hours and yy is minutes and zz is seconds
          //Two-step approach  to get hours
-            int minutes = seconds / 60;
+            if(seconds < 0){
+                return  "Invalid Data for seconds("+ seconds + "), Must be a positive interger value";
+            }
 
-            return  getDurationString(minutes,seconds);
+            return  getDurationString(seconds / 60,seconds % 60);
     }
     public static String getDurationString(int minutes ,int seconds){
          //return  'XXh YYm ZZs' where xx is hours and yy is minutes and zz is seconds
+        if(minutes < 0){
+            return  "Invalid Data for minutes("+ minutes + "), Must be a positive interger value";
+        }
+
+        if(seconds <= 0 || seconds >= 59){
+            return  "Invalid Data for seconds("+ seconds + "), Must be between 0 and 59";
+        }
         int hours  = minutes /60;
         int  remainingMinutes = minutes % 60;
-        int remainingSeconds =   seconds % 60;
-        return  hours + "h" + remainingMinutes + "m" + remainingSeconds + "s";
+        return  hours + "h" + remainingMinutes + "m" + seconds + "s";
     }
 }
