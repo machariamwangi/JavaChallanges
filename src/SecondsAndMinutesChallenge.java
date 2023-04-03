446cf66e5ca6ff419499f2e9502040ff1da4b480
ammwangi <elijah.mwangi@kamsware.com>
2023-04-03 13:21:31 +0300

    more use of overloading

--- src/SecondsAndMinutesChallenge.java
+++ src/SecondsAndMinutesChallenge.java
@@ -9,10 +9,8 @@ public class SecondsAndMinutesChallenge {
        //return  'XXh YYm ZZs' where xx is hours and yy is minutes and zz is seconds
          //Two-step approach  to get hours
             int minutes = seconds / 60;
-            int hours  = minutes /60;
-            int  remainingMinutes = minutes % 60;
-            int remainingSeconds =   seconds % 60;
-            return  hours + "h" + remainingMinutes + "m" + remainingSeconds + "s";
+
+            return  getDurationString(minutes,seconds);
     }
     public static String getDurationString(int minutes ,int seconds){
          //return  'XXh YYm ZZs' where xx is hours and yy is minutes and zz is seconds
