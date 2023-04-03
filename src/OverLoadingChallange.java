e020825401633cf487825adef9cdf40221ceb97b
ammwangi <elijah.mwangi@kamsware.com>
2023-04-03 12:55:46 +0300

    OverloadingChallange

---
+++ src/OverLoadingChallange.java
@@ -0,0 +1,21 @@
+public class OverLoadingChallange {
+
+    public static void main(String[] args) {
+        System.out.println(convertToCentimeters(5, 8));
+        System.out.println(convertToCentimeters(68));
+
+    }
+
+    public  static  double convertToCentimeters(int heightInInches){
+
+        return  heightInInches * 2.54;
+    }
+    public  static  double convertToCentimeters(int heightInFeet, int heightInInches){
+
+        int inchesofHeight = heightInFeet * 12;
+
+        double convertFeetToCentimeters = convertToCentimeters(inchesofHeight);
+        double convertInchesToCentimeters = convertToCentimeters(heightInInches);
+        return  convertFeetToCentimeters + convertInchesToCentimeters;
+    }
+}
