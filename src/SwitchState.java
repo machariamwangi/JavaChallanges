d5b7e85098dc51609bd5612f212a4d39067ab965
ammwangi <elijah.mwangi@kamsware.com>
2023-04-03 15:30:15 +0300

    enhanced switch statements in java

--- src/SwitchState.java
+++ src/SwitchState.java
@@ -2,18 +2,17 @@ public class SwitchState {
     public static void main(String[] args) {
           int switchValue = 5;
 
-        switch(switchValue){
-            case 1:
+        switch (switchValue) {
+            case 1 ->
                 //code for value ==x
-                System.out.println("Value was 1");
-                break;// the will let the execution move outside the switch block
-            case 2:
+                    System.out.println("Value was 1");
+// the will let the execution move outside the switch block
+            case 2 ->
                 //code for value
-                System.out.println("Value is 2");
-                break;
-            default:
+                    System.out.println("Value is 2");
+            default ->
                 //code for value not equal to x or y
-                System.out.println("Values is not 1 or 2");
+                    System.out.println("Values is not 1 or 2");
         }
 
     }
