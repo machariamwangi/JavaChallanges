8b4ab5fe91859094627b69ff766bc5b6aa329fb9
ammwangi <elijah.mwangi@kamsware.com>
2023-04-17 12:13:41 +0300

    for loop mini challange

--- src/ForLoopMiniChallenge.java
+++ src/ForLoopMiniChallenge.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class ForLoopMiniChallange {
+public class ForLoopMiniChallenge {
+    public static void main(String[] args) {
+            ///Using for statement , call the calculateInterest Method with the dollar amount 100.
+            /// And  this time, use the Interest rates between 7.5 and 10, but increment by a quarter of a % each time , meaning 0.25%
+
+           for (double rate = 7.5;  rate <= 10; rate += 0.25)
+           {
+               double interestAmount = calculateInterest(100.0 , rate);
+               System.out.println("100 at " + rate + "% Interest  = " + interestAmount);
+           }
+    }
+
+    //calculate Interest method
+    public  static  double calculateInterest(double amount, double interestRate){
+        return (amount * (interestRate / 100));
+    }
 }
