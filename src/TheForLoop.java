62697f8f4bfcbc7be278f1101c9756d54c4c4a5a
ammwangi <elijah.mwangi@kamsware.com>
2023-04-17 12:04:15 +0300

    for loop 2

--- src/TheForLoop.java
+++ src/TheForLoop.java
@@ -3,5 +3,18 @@ public class TheForLoop {
             for (int counter = 0; counter <= 10; counter ++){
                 System.out.println(counter);
             }
+
+            for (double rate = 2.0; rate <= 5.0; rate++){
+                double interestAmount = calculateInterest(10000.0 , rate);
+                System.out.println("10,000 at " + rate + "% Interest  = " + interestAmount);
+            }
+
+            //System.out.println("10,000 at 2% interest - " + calculateInterest(10000, 2.0));
+           // System.out.println("10,000 at 2% interest - " + calculateInterest(10000, 3.0));
+            //System.out.println("10,000 at 2% interest - " + calculateInterest(10000, 4.0));
+    }
+
+    public  static  double calculateInterest(double amount, double interestRate){
+        return (amount * (interestRate / 100));
     }
 }
